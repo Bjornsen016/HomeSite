76cf8e54690052272d0513ea64d5a676d0a8dfe4
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-02-23 21:27:00 +0100

    Orndat en simpel todo lista. Design inte fixad än.

--- javascript/main.js
+++ javascript/main.js
@@ -29,10 +29,24 @@ accordian.innerHTML = /*html*/ `
             Västtrafik
         </button>
     </h2>
-    <div id="collapseBus" class="accordion-collapse collapse show" aria-labelledby="headingBus"
+    <div id="collapseBus" class="accordion-collapse collapse" aria-labelledby="headingBus"
         data-bs-parent="#accordion">
         <div class="accordion-body" id="accordian-bus-body">
                         
+        </div>
+    </div>
+</div>
+<div class="accordion-item">
+    <h2 class="accordion-header" id="heading-handla">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse-handla" aria-expanded="true" aria-controls="collapse-handla">
+            Handlalista
+        </button>
+    </h2>
+    <div id="collapse-handla" class="accordion-collapse collapse" aria-labelledby="heading-handla"
+        data-bs-parent="#accordion">
+        <div class="accordion-body" id="accordian-handla-body">
+
         </div>
     </div>
 </div>
@@ -41,28 +55,6 @@ accordian.innerHTML = /*html*/ `
 `;
 
 main.appendChild(accordian);
-
-let accordianString = /* html */ `
-<div class="accordion" id="accordion1">
-<div class="accordion-item">
-    <h2 class="accordion-header" id="heading1">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse"
-            data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
-            Accordion Item #1
-        </button>
-    </h2>
-    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1"
-        data-bs-parent="#accordion1">
-        <div class="accordion-body">
-            <img class="img-fluid" src="https://www.yr.no/en/content/2-2696644/meteogram.svg" alt="Prognos inte hittad">
-            <iframe
-            src="https://avgangstavla.vasttrafik.se/?source=vasttrafikse-stopareadetailspage&stopAreaGid=9021014017320000"
-            frameborder="1"></iframe>
-            </div>
-    </div>
-</div>
-</div>
-`;
 
 async function getToken() {
 	const json = await fetch("https://api.vasttrafik.se/token", {
@@ -76,10 +68,6 @@ async function getToken() {
 	return json;
 }
 
-const token = await getToken();
-
-console.log(token);
-
 async function getDepBoard(token, stopId, stopIdWhereTo) {
 	("9022014005279002"); /* pilgatan id */
 	("9022014017320004"); /* gråbo id */
@@ -87,11 +75,6 @@ async function getDepBoard(token, stopId, stopIdWhereTo) {
 
 	let now = new Date().toLocaleTimeString();
 	let today = new Date().toLocaleDateString();
-	console.log(now);
-	console.log(today);
-	/* let lat = "57.843803299198406";
-	let long = "12.291675805750685";
-	let urlGråbo = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=${stopId}&date=${today}&time=18%3A20&direction=9022014005279002&format=json`; */
 
 	const url = new URL("https://api.vasttrafik.se");
 	url.pathname = "/bin/rest.exe/v2/departureBoard";
@@ -100,8 +83,6 @@ async function getDepBoard(token, stopId, stopIdWhereTo) {
 	url.searchParams.set("time", now);
 	url.searchParams.set("direction", stopIdWhereTo);
 	url.searchParams.set("format", "json");
-
-	/* let urll = `https://api.vasttrafik.se/bin/rest.exe/v2/location.nearbystops?originCoordLat=${lat}&originCoordLong=${long}&format=json`; */
 
 	const data = await fetch(url, {
 		metod: "GET",
@@ -137,5 +118,109 @@ async function getDepBoard(token, stopId, stopIdWhereTo) {
 	});
 	accordianBus.appendChild(card);
 }
+
+async function logInToTodoApp() {
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var raw = JSON.stringify({
+		email: "kim.bjornsen@hotmail.com",
+		password: "laika016",
+	});
+
+	var requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: raw,
+		redirect: "follow",
+	};
+
+	const todoToken = await fetch(
+		"https://api-nodejs-todolist.herokuapp.com/user/login",
+		requestOptions
+	)
+		.then((response) => response.json())
+		.catch((error) => console.log("error", error));
+
+	console.log(todoToken);
+	return todoToken.token;
+}
+
+async function addTodo(token, whatToDo) {
+	var myHeaders = new Headers();
+	myHeaders.append("Authorization", `Bearer ${token}`);
+	myHeaders.append("Content-Type", "application/json");
+
+	var raw = JSON.stringify({
+		description: whatToDo,
+	});
+
+	var requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: raw,
+		redirect: "follow",
+	};
+
+	fetch("https://api-nodejs-todolist.herokuapp.com/task", requestOptions)
+		.then((response) => response.json())
+		.then((result) => console.log(result))
+		.catch((error) => console.log("error", error));
+}
+async function getTodos(token) {
+	var myHeaders = new Headers();
+	myHeaders.append("Authorization", `Bearer ${token}`);
+	myHeaders.append("Content-Type", "application/json");
+
+	var requestOptions = {
+		method: "GET",
+		headers: myHeaders,
+		redirect: "follow",
+	};
+
+	const data = await fetch(
+		"https://api-nodejs-todolist.herokuapp.com/task",
+		requestOptions
+	)
+		.then((response) => response.json())
+		.catch((error) => console.log("error", error));
+	console.log(data);
+	return data;
+}
+
+function printTodos(todoList) {
+	const todoHtmlEl = document.getElementById("accordian-handla-body");
+	const todoUl = document.createElement("ul");
+	todoUl.classList.add("list-group");
+
+	todoList.forEach((todo) => {
+		const liEl = document.createElement("li");
+		liEl.id = todo._id;
+		liEl.classList.add("list-group-item");
+
+		let desc = document.createElement("div");
+		desc.innerText = `${todo.description}`;
+		liEl.appendChild(desc);
+
+		let compl = document.createElement("div");
+		compl.innerText = `${todo.completed}`;
+		liEl.appendChild(compl);
+
+		/* liEl.append(`<p>${todo.description}</p>`, `<p>${todo.completed}</p>`); */
+		todoUl.appendChild(liEl);
+	});
+	todoHtmlEl.appendChild(todoUl);
+}
+
+const token = await getToken();
+
+console.log(token);
+
 await getDepBoard(token, 9022014017320004, 9022014005279002);
 getDepBoard(token, 9022014005279002, 9022014017320004);
+
+const todoToken = await logInToTodoApp();
+
+const todos = await getTodos(todoToken);
+
+printTodos(todos.data);
