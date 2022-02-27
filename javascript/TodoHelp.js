2439cdab129f5dd51c0ac1d3fc188b71a0298484
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-02-27 15:13:29 +0100

    Att göra lista ordnad lite till

--- javascript/TodoHelp.js
+++ javascript/TodoHelp.js
@@ -23,11 +23,11 @@ export function toggleCompleteTodo(id, elem, compl, token) {
 
 			if (list.contains("false")) {
 				elem.classList.replace("false", "true");
-				elem.classList.replace("btn-danger", "btn-success");
+				elem.classList.replace("bg-danger", "bg-success");
 				elem.innerText = "Gjord: Ja";
 			} else if (list.contains("true")) {
 				elem.classList.replace("true", "false");
-				elem.classList.replace("btn-success", "btn-danger");
+				elem.classList.replace("bg-success", "bg-danger");
 				elem.innerText = "Gjord: Nej";
 			}
 
@@ -108,7 +108,7 @@ function createTable(list, token, header) {
 	const thead = document.createElement("thead");
 	const trhead = document.createElement("tr");
 
-	const headingArr = ["Att göra", "Gjort", "Radera"];
+	const headingArr = ["Att göra", "Radera"];
 	headingArr.forEach((str) => {
 		const th = document.createElement("th");
 		th.setAttribute("scope", "col");
@@ -124,19 +124,23 @@ function createTable(list, token, header) {
 	list.forEach((todo) => {
 		const tableRow = document.createElement("tr");
 
-		const contentArr = [todo.description, todo.completed, "Radera"];
+		const contentArr = [[todo.description, todo.completed], "Radera"];
 
 		contentArr.forEach((str, index) => {
 			const td = document.createElement("td");
 			td.style.fontSize = "1.2rem";
-			if (index === 1) {
+			if (index === 0) {
+				td.setAttribute("scope", "row");
 				const div = document.createElement("div");
-				div.classList.add("form-check", "form-switch");
+				div.classList.add("form-check");
 				const input = document.createElement("input");
-				input.classList.add("form-check-input", str);
+
+				if (str[1] === true)
+					input.classList.add("form-check-input", str[1], "bg-success");
+				else input.classList.add("form-check-input", str[1], "bg-danger");
 				input.setAttribute("type", "checkbox");
 				input.id = todo._id;
-				input.checked = str;
+				input.checked = str[1];
 				input.addEventListener("click", (event) => {
 					console.log(event.target);
 					let boolean;
@@ -145,12 +149,18 @@ function createTable(list, token, header) {
 					else boolean = true;
 					toggleCompleteTodo(event.target.id, event.target, boolean, token);
 				});
+
+				const label = document.createElement("label");
+				label.classList.add("form-check-label");
+				label.setAttribute("for", todo._id);
+				label.innerText = str[0];
+
 				div.appendChild(input);
+				div.appendChild(label);
 				td.appendChild(div);
-			} else if (index === 2) {
+			} else if (index === 1) {
 				const button = document.createElement("button");
-				button.classList.add("btn", "btn-danger");
-				button.style.borderRadius = "50%";
+				button.classList.add("btn", "btn-danger", "btn-sm");
 				button.id = todo._id;
 				button.innerText = "X";
 
@@ -169,10 +179,9 @@ function createTable(list, token, header) {
 					printTodos(newList.data, token, header);
 				});
 
+				td.classList.add("justify-content-center");
+				td.style.display = "flex";
 				td.appendChild(button);
-			} else {
-				td.setAttribute("scope", "row");
-				td.innerText = str;
 			}
 
 			tableRow.appendChild(td);
